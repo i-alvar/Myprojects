742fe4f5e1a6d7309edbef35fc9fdfe2072b4767
i-alvar <ialvare@proton.me>
2026-06-13 01:34:26 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  // This tells Vite to look for assets in your repository sub-folder
+  base: '/Myprojects/',
 })
-
